4adb8ccc6bc3567278e24833fe1bf40fbf1d82e4
Arthur Deschamps <arthur.deschamps1208@hotmail.com>
2017-06-22 19:09:51 +0200

    no message

--- src/main/java/kapua/gateway/Client.java
+++ src/main/java/kapua/gateway/Client.java
@@ -1,9 +1,50 @@
 package kapua.gateway;
 
+import kapua.gateway.*;
+import org.eclipse.kapua.gateway.client.Application;
+import org.eclipse.kapua.gateway.client.Payload;
+import org.eclipse.kapua.gateway.client.Sender;
+import org.eclipse.kapua.gateway.client.Topic;
+import org.eclipse.kapua.gateway.client.mqtt.fuse.FuseClient;
+
 /**
  * Description..
  *
  * @author Arthur Deschamps
  */
 public class Client {
+
+    public Client() {
+        try (Client client = KuraMqttProfile.newProfile(FuseClient.Builder::new)
+                .accountName("kapua-sys")
+                .clientId("foo-bar-1")
+                .brokerUrl("tcp://localhost:1883")
+                .credentials(userAndPassword("kapua-broker", "kapua-password"))
+                .build()) {
+
+            try (Application application = client.buildApplication("app1").build()) {
+
+                // subscribe to a topic
+
+                application.data(Topic.of("my", "receiver")).subscribe(message -> {
+                    System.out.format("Received: %s%n", message);
+                });
+
+                // cache sender instance
+
+                Sender<RuntimeException> sender = application
+                        .data(Topic.of("my", "sender"))
+                        .errors(ignore());
+
+                int i = 0;
+                while (true) {
+                    // send
+                    sender.send(Payload.of("counter", i++));
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
