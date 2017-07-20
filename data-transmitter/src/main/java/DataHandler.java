b2ccea12620fc03fa3b93bc39d2cf5e654676dc8
Arthur Deschamps <arthur.deschamps1208@hotmail.com>
2017-07-20 14:19:22 +0200

    Add decoder in MqttListener for messages received from subscriptions.

---
+++ data-transmitter/src/main/java/DataHandler.java
@@ -0,0 +1,7 @@
+/**
+ * Handles maps received through the subscription mechanism.
+ * @since 1.0
+ * @author Arthur Deschamps
+ */
+public class DataHandler {
+}
--- data-transmitter/src/main/java/MqttListener.java
+++ data-transmitter/src/main/java/MqttListener.java
@@ -2,30 +2,38 @@ import org.eclipse.paho.client.mqttv3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.eclipse.kapua.gateway.client.Payload;
+import org.eclipse.kapua.gateway.client.kura.internal.Metrics;
+import org.eclipse.kapua.gateway.client.kura.payload.KuraPayloadProto.KuraPayload;
+
 /**
- * Handler on received messages from subscriptions
+ * Mqtt client callback handler.
  * @author Arthur Deschamps
  */
 class MqttListener implements MqttCallbackExtended {
 
     private static final Logger logger = LoggerFactory.getLogger(MqttListener.class);
 
+    private DataHandler dataHandler;
+
     private IMqttAsyncClient client;
-    private String clientId;
     private String publisherId;
     private String applicationId;
     private String publisherAccountName;
 
     public MqttListener(IMqttAsyncClient client, String clientId, String publisherId, String applicationId, String publisherAccountName) {
         this.client = client;
-        this.clientId = clientId;
         this.publisherId = publisherId;
         this.applicationId = applicationId;
         this.publisherAccountName = publisherAccountName;
+        this.dataHandler = new DataHandler();
     }
 
     @Override
@@ -49,9 +57,9 @@ class MqttListener implements MqttCallbackExtended {
 
     @Override
     public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-        // s is the topic
+        // Decode the key-value pair
+        Payload payload = decode(mqttMessage.getPayload());
         logger.info(s);
-        logger.info(mqttMessage.getPayload().toString();
     }
 
     @Override
@@ -63,10 +71,34 @@ class MqttListener implements MqttCallbackExtended {
         }
     }
 
+    /**
+     * Assembles a proper topic for kapua.
+     * @param topic
+     * The raw topic, for instance ["foo"] for topic "foo", ["foo", "bar"] for topic "foo/bar"
+     * @return
+     * A well formed topic that can be used to subscribe to the kapua broker.
+     */
     private String topic(final String... topic) {
         return Stream.concat(
                 Stream.of(publisherAccountName,publisherId, applicationId),
                 Arrays.stream(topic))
                 .collect(Collectors.joining("/"));
+    }
+
+    /**
+     * Decodes an MqttMessage payload sent by the kapua broker.
+     * @param data
+     * The received message payload.
+     * @return
+     * A map containing one or multiple key-value pairs.
+     * @throws Exception
+     * KuraPayload.parseFrom() might potentially raise an exception.
+     */
+    private Payload decode(final byte[] data) throws Exception {
+        Objects.requireNonNull(data);
+
+        final KuraPayload payload = KuraPayload.parseFrom(data);
+        final Map<String, Object> values = Metrics.extractMetrics(payload);
+        return Payload.of(Instant.ofEpochMilli(payload.getTimestamp()), values);
     }
 }
