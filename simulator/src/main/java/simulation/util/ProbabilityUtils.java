f1a9a38434ee60902eaf21ec144686d0690b2268
Arthur Deschamps <arthur.deschamps1208@hotmail.com>
2017-08-01 14:21:08 +0200

    Update copyright in all files. Update gitignore. Add about.html in each module and license in whole project.

--- simulator/src/main/java/simulation/util/ProbabilityUtils.java
+++ simulator/src/main/java/simulation/util/ProbabilityUtils.java
@@ -1,3 +1,17 @@
+/*
+ * ******************************************************************************
+ *  * Copyright (c) 2017 Arthur Deschamps
+ *  *
+ *  * All rights reserved. This program and the accompanying materials
+ *  * are made available under the terms of the Eclipse Public License v1.0
+ *  * which accompanies this distribution, and is available at
+ *  * http://www.eclipse.org/legal/epl-v10.html
+ *  *
+ *  * Contributors:
+ *  *     Arthur Deschamps
+ *  ******************************************************************************
+ */
+
 package simulation.util;
 
 import java.util.Random;
--- webapp-angular/lib/src/data_services/iot/IotDataClient.service.dart
+++ webapp-angular/lib/src/data_services/iot/IotDataClient.service.dart
@@ -1,5 +1,16 @@
-// Copyright (c) 2017, arthurdeschamps. All rights reserved. Use of this source code
-// is governed by a BSD-styles license that can be found in the LICENSE file.
+/*
+ * ******************************************************************************
+ *  * Copyright (c) 2017 Arthur Deschamps
+ *  *
+ *  * All rights reserved. This program and the accompanying materials
+ *  * are made available under the terms of the Eclipse Public License v1.0
+ *  * which accompanies this distribution, and is available at
+ *  * http://www.eclipse.org/legal/epl-v10.html
+ *  *
+ *  * Contributors:
+ *  *     Arthur Deschamps
+ *  ******************************************************************************
+ */
 
 import 'package:angular2/angular2.dart';
 import 'dart:html';
