7c5cd13754bbc02f080db06e3fa36b64bca7f746
Arthur Deschamps <arthur.deschamps1208@hotmail.com>
2017-08-16 17:59:42 +0200

    Add new script for dart testing

---
+++ webapp-angular/test/test_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+DART_DIST=dartsdk-linux-x64-release.zip
+DARTIUM_DIST=dartium-linux-x64-release.zip
+
+echo Fetching dart sdk and Dartium
+curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
+curl http://storage.googleapis.com/dart-archive/channels/stable/raw/latest/dartium/$DARTIUM_DIST > $DARTIUM_DIST
+
+unzip $DART_DIST > /dev/null
+unzip $DARTIUM_DIST > /dev/null
+rm $DART_DIST
+rm $DARTIUM_DIST
+
+mv dartium-* dartium;
+
+export DART_SDK="$PWD/dart-sdk"
+export PATH="$DART_SDK/bin:$PATH"
+export DARTIUM_BIN="$PWD/dartium/chrome"
+
+echo Pub install
+pub install
+
+sh -e /etc/init.d/xvfb start
+
+cd ../
+pub run test -p dartium test/
